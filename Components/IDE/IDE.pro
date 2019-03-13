90d2686165bf5d46d10d2374d498be358849bff2
Alex Keel <alexkeel@hotmail.co.uk>
2019-03-13 22:08:55 +0000

    added basic text editor

--- Components/IDE/IDE.pro
+++ Components/IDE/IDE.pro
@@ -29,3 +29,6 @@ SOURCES += \
 
 HEADERS += \
         mainwindow.h
+
+RESOURCES += \
+    application.qrc
--- Components/IDE/main.cpp
+++ Components/IDE/main.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Q_INIT_RESOURCE(application);
 
-    return a.exec();
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("KeelStudios");
+    QCoreApplication::setApplicationName("Brusky Edit");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    MainWindow mainWin;
+    if(!parser.positionalArguments().isEmpty())
+    {
+        mainWin.loadFile(parser.positionalArguments().first());
+    }
+    mainWin.show();
+    return app.exec();
 }
